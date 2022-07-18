f398f21d50b68cc2ad199b0a212d08dcc5d2cee4
Rob <rfrank@filamentgames.com>
2022-07-18 11:00:48 -0500

    One more cocoapods fix. Match icivics

--- Assets/_Wrapper/Scripts/Editor/CICocoaPodsFix.cs
+++ Assets/_Wrapper/Scripts/Editor/CICocoaPodsFix.cs
@@ -1,14 +1,15 @@
-#if UNITY_IOS
-using UnityEditor.iOS.Xcode;
-#endif
+
 using UnityEditor;
 using UnityEditor.Callbacks;
+#if UNITY_EDITOR_OSX
+	using UnityEditor.iOS.Xcode;
+#endif
 
 namespace Editor
 {
     public static class XcodeSwiftVersionPostProcess
     {
-#if UNITY_IOS
+#if UNITY_EDITOR_OSX
         [PostProcessBuild(999)]
         public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
         {
@@ -17,21 +18,21 @@ namespace Editor
                 ModifyFrameworks(path);
             }
         }
- 
+
         private static void ModifyFrameworks(string path)
         {
             string projPath = PBXProject.GetPBXProjectPath(path);
-           
+
             var project = new PBXProject();
             project.ReadFromFile(projPath);
- 
+
             string targetGuid = project.GetUnityFrameworkTargetGuid();
-           
+
             project.SetBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "NO");
-           
+
             //string mainTargetGuid = project.GetUnityMainTargetGuid();
             //project.SetBuildProperty(mainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
- 
+
             project.WriteToFile(projPath);
         }
 #endif
