740df4de0d8ade6be541abed88ba5a1e8d20e420
gcwill <gcwillgcwill@gmail.com>
2023-12-05 16:18:05 -0600

    Implemented level locking

--- Assets/Circuits/Scripts/Circuits_SaveData.cs
+++ Assets/Circuits/Scripts/Circuits_SaveData.cs
@@ -9,6 +9,7 @@ namespace Circuits
     {
         public Wrapper.Game gameID = Wrapper.Game.Circuits;
         public int currLevel = 0;
+        public int maxLevel = 0;
         public bool[] completedLevels = new bool[CTConstants.N_LEVELS];
         public int[] starsPerLevel = new int[CTConstants.N_LEVELS];
         public int totalStars = 0;
