f096d6bc277e5cbe115415cb6397232cd8a2d6d4
Jonathan Liu <liujon23@gmail.com>
2024-08-22 17:53:30 -0500

    Hint button prints something

---
+++ Assets/Qupcakes/Scripts/UI/GeneralButtons/HintButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Qupcakery
+{
+    public class HintButton : MonoBehaviour
+    {
+        public void GiveHint()
+        {
+            print("abc");
+        }
+    }
+}
+
