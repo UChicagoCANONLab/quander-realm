76ad6aeac6cde2fb28dc6e6f7e25f49989f79109
gcwill <gcwillgcwill@gmail.com>
2025-01-24 21:57:37 -0600

    Fixed issues from merging

--- Assets/_Wrapper/Scripts/QButtons (Subclassed Buttons)/BackButton.cs
+++ Assets/_Wrapper/Scripts/QButtons (Subclassed Buttons)/BackButton.cs
@@ -13,6 +13,24 @@ namespace Wrapper
 
             if (SceneManager.GetActiveScene().name == "QU_Level")
             {
+                Time.timeScale = 1;
+            }
+
+            if (SceneManager.GetActiveScene().buildIndex == 0) 
+            {
+                Events.ToggleTitleScreen?.Invoke(true);
+            }
+            else
+            {
+                Events.ScreenFadeMidAction?.Invoke(() =>
+                {
+                    SceneManager.LoadScene(0);
+                    Events.MinigameClosed?.Invoke();
+                }, 0.1F);
+            }
+
+            /* if (SceneManager.GetActiveScene().name == "QU_Level")
+            {
                 GameUtilities.UnpauseGame();
                 GameManagement.Instance.game.gameStat.SetLevelResultAndSave(GameStat.LevelResult.QUIT);
                 GameObjectsManagement.ResetAllGameObjects();
@@ -23,7 +41,7 @@ namespace Wrapper
                 return;
 
             SceneManager.LoadScene(0);
-            Events.MinigameClosed?.Invoke();
+            Events.MinigameClosed?.Invoke(); */
         }
     }
 }
