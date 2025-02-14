89d88a50a83386de84868ff5042862736a6cac06
gcwill <gcwillgcwill@gmail.com>
2025-02-14 16:04:48 -0600

    Updated levels when rewards unlocked

--- Assets/Qupcakes/Scripts/Rewards/RewardManager.cs
+++ Assets/Qupcakes/Scripts/Rewards/RewardManager.cs
@@ -9,14 +9,16 @@ namespace Qupcakery
     {
         public static Dictionary<int, string> rewardsInd = new Dictionary<int, string>
         {
-            { 9,    "QU_01" },
+            { 10,   "QU_01" },
             { 27,   "QU_02" },
             { 1,    "QU_03" },
             { 23,   "QU_04" },
             { 5,    "QU_05" },
             { 13,   "QU_06" },
             { 20,   "QU_07" },
-            { 15,   "QU_08" }
+            { 15,   "QU_08" },
+            { 8,    "QU_09" },
+            { 3,    "QU_10" }
         };
     }
 }
