c69fa97c344f7da8c2547465be249637d2da1b12
gcwill <gcwillgcwill@gmail.com>
2025-03-06 15:03:16 -0600

    Reorganized Events to label script where event defined

--- Assets/_Wrapper/Scripts/GameManagement/Events.cs
+++ Assets/_Wrapper/Scripts/GameManagement/Events.cs
@@ -7,41 +7,54 @@ namespace Wrapper
 {
     public static class Events
     {
-        /// Game Management ///
+    #region Game Management
+
+        /// GameManager ///
         public static Action<Minigame> OpenMinigame;
-        public static Action MinigameClosed;
+        public static Func<RewardAsset, GameObject, DisplayType, GameObject> CreatRewardCard;
+        public static Action<string> ShowCardPopup;
         public static Action ToggleLoadingScreen;
-        public static Action OpenLoginScreen;
-        public static Action CloseLoginScreen;
-        public static Action<bool> ToggleBackButton;
-        public static Action<bool> ToggleTitleScreen;
-        public static Action PlayIntroDialog;
-        public static Func<Game> GetCurrentGame;
-        public static Func<Game, string> GetMinigameTitle;
+        public static Action<Game, int> CollectAndDisplayReward;
         public static Action<Game> UnlockAndDisplayGame;
         public static Action DisplayAgeSelector;
+        public static Action<bool> ToggleBackButton;
+        public static Action Logout;
+        public static Action PlayIntroDialog;
+        public static Action MinigameClosed;
+        public static Func<Game, string> GetMinigameTitle;
+        public static Func<Game> GetCurrentGame;
+        public static Func<bool> IsDebugEnabled;
+
+        /// Trackers ///
         public static Action InitializeStarTracker;
-        public static Action ResetStarCounts;
         public static Action<long> UpdateStreakLength;
+        public static Action ResetStarCounts;
+
+        /// MapManager ///
         public static Action InitializeMap;
         public static Action ResetMap;
 
-        /// Screen Fading ///
-        public static Action<Action, float> ScreenFadeMidAction;
-        public static Action StopScreenFade;
-
-        /// Save System ///
-        public static Action<string> SubmitResearchCode;
-        public static Action<LoginStatus> UpdateLoginStatus;
-        public static Action<Game, object> UpdateMinigameSaveData;
-        public static Func<Game, string> GetMinigameSaveData;
-        public static Action UpdateRemoteSave;
-        public static Action ClearSaveFile;
-        public static Action<bool> ToggleUploadFailurePopup;
-        public static Func<string> GetPlayerResearchCode;
-        public static Action<Game, object> SaveMinigameResearchData;
+        /// TitleScreen ///
         public static Action<bool> SetNewPlayerStatus;
-        public static Action Logout;
+        public static Action<bool> ToggleTitleScreen;
+
+    #endregion
+
+    #region Save System
+
+        /// SaveManager ///
+        public static Func<string, bool> AddReward;
+        public static Action ClearSaveFile;
+        public static Action<string> SubmitResearchCode;
+        public static Func<string, bool> IsRewardUnlocked;
+        public static Action UpdateRemoteSave;
+        public static Func<string> GetPlayerResearchCode;
+        public static Func<Game, string> GetMinigameSaveData;
+        public static Action<Game, object> UpdateMinigameSaveData;
+        public static Action<Game, object> SaveMinigameResearchData;
+        public static Func<(bool, bool)> GetRewardDialogStats;
+        public static Action<bool> SetRewardTextSeen;
+        public static Func<string, bool> GetFirstRewardBool;
         public static Action<int> UpdateUserSaveTotalStars;
         public static Action<int> UpdateUserSaveTotalCoins;
         public static Func<int> GetUserSaveTotalCoins;
@@ -49,49 +62,7 @@ namespace Wrapper
         public static Func<Game, int> GetMinigameStarCount;
         public static Func<int> GetStreakLength;
 
-        /// Dialogue System ///
-        public static Action<string> PrintDialogue;
-        public static Action SortSequences;
-        public static Action<string> StartDialogueSequence;
-        public static Action<Dialogue> OpenDialogueView;
-        public static Action<Dialogue, int> UpdateDialogueView;
-        public static Action CloseDialogueView;
-        public static Action DialogueSequenceEnded;
-        public static Action<int> ChangeDialogue;
-        public static Action<bool> SwitchNextButton;
-        public static Action<bool> TogglePreviousButton;
-        public static Action EnableSkipButton;
-
-        /// Reward System ///
-        public static Func<string, bool> AddReward;
-        public static Func<string, bool> IsRewardUnlocked;
-        public static Action<JournalPage> OpenJournalPage;
-        public static Action<string> FeatureCard;
-        public static Action ResetPageNumbers;
-        public static Func<int, Toggle> GetNavDot;
-        public static Action<JournalPage> SwitchPage;
-        public static Action UnselectAllCards;
-        public static Action<JournalPage> UpdateTab;
-        public static Action<Game, int> CollectAndDisplayReward;
-        public static Func<RewardAsset, GameObject, DisplayType, GameObject> CreatRewardCard;
-        public static Action<bool> SetRewardTextSeen;
-        public static Func<(bool, bool)> GetRewardDialogStats;
-        public static Func<string, bool> GetFirstRewardBool;
-        public static Func<bool> ReturnToRewardCenter;
-
-        /// Audio System ///
-        public static Action<string> PlayMusic;
-        public static Action StopMusic;
-        public static Action<string> PlaySound;
-
-        /// Debug ///
-        public static Action<string> BBGotoLevel;
-        public static Action BBToggleDebug;
-        public static Action BBClearMarkers;
-        public static Action<string> ShowCardPopup;
-        public static Func<bool> IsDebugEnabled;
-
-        /// Minigame User Saves ///
+        /// MinigameUserSaves ///
         public static Func<Game, int> GetMinigameTotalStars;
         public static Func<int> GetOverallTotalStars;
         public static Func<Game, bool> GetGameUnlocked;
@@ -99,5 +70,81 @@ namespace Wrapper
         public static Action<Game> LoadMinigameSave;
         public static Action LoadAllMinigameSaves;
 
+        /// UploadFailurePopup s/// 
+        public static Action<bool> ToggleUploadFailurePopup;
+
+        /// LoginScreen OR QuantimeLoginScreen ///
+        public static Action OpenLoginScreen;
+        public static Action<LoginStatus> UpdateLoginStatus;
+        public static Action CloseLoginScreen;
+
+    #endregion
+
+    #region Dialogue System
+
+        /// DialogueManager ///
+        public static Action<string> PrintDialogue;
+        public static Action<string> StartDialogueSequence;
+        public static Action<int> ChangeDialogue;
+
+        /// DialogueView ///
+        public static Action<Dialogue> OpenDialogueView;
+        public static Action CloseDialogueView;
+        public static Action<Dialogue, int> UpdateDialogueView;
+        public static Action<bool> SwitchNextButton;
+
+        /// DialogueSequence ///
+        public static Action SortSequences;
+
+        /// DialogueButton ///
+        public static Action<bool> TogglePreviousButton;
+        public static Action EnableSkipButton;
+
+        /// Within Minigame Scripts ///
+        public static Action DialogueSequenceEnded;
+
+    #endregion
+
+    #region Reward System
+
+        /// RewardCenter ///
+        public static Func<bool> ReturnToRewardCenter;
+
+        /// Reward ///
+        public static Action UnselectAllCards;
+
+        /// RewardJournal ///
+        public static Func<int, Toggle> GetNavDot;
+        public static Action<JournalPage> SwitchPage;
+        public static Action<string> FeatureCard;
+        // public static Action<JournalPage> OpenJournalPage;
+
+        /// JournalSection ///
+        public static Action<JournalPage> UpdateTab;
+        public static Action ResetPageNumbers;
+
+    #endregion
+
+    #region Screen Fading, Audio System
+    
+        /// AudioManager ///
+        public static Action<string> PlayMusic;
+        public static Action StopMusic;
+        public static Action<string> PlaySound;
+
+        /// ScreenFader ///
+        public static Action<Action, float> ScreenFadeMidAction;
+        public static Action StopScreenFade;
+    
+    #endregion
+
+    #region Debug
+
+        /// ??? ///
+        public static Action<string> BBGotoLevel;
+        public static Action BBToggleDebug;
+        public static Action BBClearMarkers;
+
+    #endregion
     }
 }
