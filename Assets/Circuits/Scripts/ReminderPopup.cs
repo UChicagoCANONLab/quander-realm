83e3da4361262302a8e116149bb4b44626f52224
gcwill <gcwillgcwill@gmail.com>
2025-03-31 17:27:32 -0500

    Altered button animation, initialized reminder popup

---
+++ Assets/Circuits/Scripts/ReminderPopup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Circuits
+{
+    public class ReminderPopup : MonoBehaviour
+    {
+        public GameObject hintButton;
+        public GameObject infoButton;
+
+        
+    }
+}
