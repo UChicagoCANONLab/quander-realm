31a91aff74bcb46ea302e7a0c36c42302ed60f0b
Mercy Omwoyo <mercyomwoyo@Mercys-MacBook-Air.local>
2026-05-04 00:55:48 -0500

    Add Computer Minigame landing page to RewardCenter
    - Added ComputerMinigame button on left monitor
    - Added ComputerMinigame_View with title, two computer cards
    - Added openComputerMinigame(), openSuperconducting(), openNeutralAtom()
    - Added CM_Intro dialogue sequence
    - Added placeholder computer illustrations

--- Assets/_Wrapper/Scripts/RewardSystem/RewardCenter.cs
+++ Assets/_Wrapper/Scripts/RewardSystem/RewardCenter.cs
@@ -13,6 +13,7 @@ namespace Wrapper
         // [SerializeField] private GameObject PuzzlesOfTheDay; // left blue screen
         [SerializeField] private GameObject BadgeBulletinCanvas; // right blue screen
         [SerializeField] private GameObject RewardJournalCanvas; // journal
+        [SerializeField] private GameObject ComputerMinigameCanvas; // Left monitor
 
         [SerializeField] private GeneralTrackers generalTrackers;
 
@@ -64,6 +65,27 @@ namespace Wrapper
             BadgeBulletinCanvas.GetComponent<BadgeManager>().DelayGetStars();
         }
 
+        //Called by ComputerMinigame OnClick
+        //Fades out the lab and opens the computer minigame landing page
+        public void openComputerMinigame() {
+            RewardCenterAnimator.SetTrigger("Fader");// fade transition
+            RewardCenterAnimator.SetBool("On", false);//hide lab
+            // TODO: ComputerMinigameCanvas.GetComponent<Animator>().SetBool("On", true);
+            ComputerMinigameCanvas.SetActive(true);
+            Events.StartDialogueSequence?.Invoke("CM_Intro");
+            Debug.Log("Computer Minigame opened"); // temporary test
+        }
+        // Called by SuperconductingCard OnClick
+        // Opens the Superconducting assembly panel
+        public void openSuperconducting() {
+            Debug.Log("Superconducting selected");
+        }
+
+        // Called by NeutralAtomCard OnClick
+        // Opens the Neutral Atom assembly panel
+        public void openNeutralAtom() {
+            Debug.Log("Neutral Atom selected");
+        }
 
         public void returnToRewardCenter() {
             Events.Delay?.Invoke(0.5f);
@@ -76,6 +98,8 @@ namespace Wrapper
                 BadgeBulletinCanvas.GetComponent<Animator>().SetBool("On", false);
 
                 RewardCenterAnimator.SetBool("On", true);
+
+                // TODO: close ComputerMinigameCanvas here once canvas exists
             } 
         }
 
